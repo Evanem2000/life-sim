62185874ebcbc5eaa2d685ef5156f279bda4c65b
Evan Stasiewicz <estasiewicz22@georgefox.edu>
2025-11-22 00:13:18 -0800

    Added index page

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// Redirect to login or signup when buttons are clicked
+document.getElementById('login-btn').addEventListener('click', () => {
+  window.location.href = 'login.html';
+});
+
+document.getElementById('signup-btn').addEventListener('click', () => {
+  window.location.href = 'signup.html';
+});
+
+console.log("Index page loaded. Buttons ready.");
